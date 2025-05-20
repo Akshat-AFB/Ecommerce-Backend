5a6855bc1a61867418007ea6297f90067abf68c6
akshat <akshat@admins-MacBook-Pro.local>
2025-05-21 04:15:26 +0530

    Product functions added

--- Ecommerce/main.py
+++ Ecommerce/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routes import userRoutes
+from routes import userRoutes, productRoutes
 
 app = FastAPI(title="E-Commerce Backend", version="1.0")
 
 
-app.include_router(userRoutes.router, prefix="/users", tags=["Users"])
-# app.include_router(product_routes.router, prefix="/products", tags=["Products"])
+app.include_router(userRoutes.router, prefix="/users")
+app.include_router(productRoutes.router, prefix="/products")
 # app.include_router(cart_routes.router, prefix="/cart", tags=["Cart"])
 # app.include_router(order_routes.router, prefix="/orders", tags=["Orders"])
---
+++ Ecommerce/repositories/product_repo.py
@@ -0,0 +1,30 @@
+class productRepository:
+
+    products = []
+    curr_id = 1
+
+    @classmethod
+    def create_product(cls, product):
+        product_data = {
+            "id": cls.curr_id,
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "brand": product.brand,
+            "stock_quantity": product.stock_quantity,
+            "image_url": product.image_url
+        }
+        cls.products.append(product_data)
+        cls.curr_id += 1
+        return product_data
+
+    @classmethod
+    def list_products(cls, offset: int = 0, limit: int = 10):
+        return cls.products[offset: offset + limit]
+    
+    @classmethod
+    def get_product(cls, product_id: int):
+        for product in cls.products:
+            if product["id"] == product_id:
+                return product
+        return None
